4c036d6d92c23bbb2cfabbe09596e1eab769482d
Manas Mondal <manasmondal035@gmail.com>
2026-04-27 17:44:16 +0530

    migrate chatbot from HTML to React

---
+++ src/components/ChatInput.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { Chatbot } from "supersimpledev";
+import SpinnerProfileImage from "../assets/loading-spinner.gif";
+import "./ChatInput.css";
+
+export function ChatInput({ chatMessages, setChatMessages }) {
+  const [inputText, setInputText] = useState("");
+  const [isLoading, setIsloading] = useState(false);
+  function saveInputText(event) {
+    setInputText(event.target.value);
+  }
+  async function sendMessage() {
+    if (isLoading || inputText === "") {
+      return;
+    }
+    setIsloading(true);
+    setInputText("");
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: "user",
+        id: crypto.randomUUID(),
+      },
+    ];
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: <img className="spinner-img" src={SpinnerProfileImage} />,
+        sender: "robot",
+        id: crypto.randomUUID(),
+      },
+    ]);
+
+    const response = await Chatbot.getResponseAsync(inputText);
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: response,
+        sender: "robot",
+        id: crypto.randomUUID(),
+      },
+    ]);
+    setIsloading(false);
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      sendMessage();
+    } else if (event.key === "Escape") {
+      setInputText("");
+    }
+  }
+
+  return (
+    <div className="chat-input-container">
+      <input
+        placeholder="Send a message to Chatbot"
+        size="30"
+        onChange={saveInputText}
+        value={inputText}
+        onKeyDown={handleKeyDown}
+        className="chat-input"
+      />
+      <button onClick={sendMessage} className="send-button">
+        Send
+      </button>
+    </div>
+  );
+}
---
+++ src/components/ChatMessages.jsx
@@ -0,0 +1,23 @@
+import { ChatMessage } from "./ChatMessage";
+import useAutoScroll from "../hooks/useAutoScroll";
+import "./ChatMessages.css";
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useAutoScroll([chatMessages]);
+
+  return (
+    <div className="chat-messages-container" ref={chatMessagesRef}>
+      {chatMessages.map((chat) => {
+        return (
+          <ChatMessage
+            message={chat.message}
+            sender={chat.sender}
+            key={chat.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default ChatMessages;
