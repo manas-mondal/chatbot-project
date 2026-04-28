176911f0c48e5a50785177f1958e4d2afc486423
Manas Mondal <manasmondal035@gmail.com>
2026-04-28 13:08:17 +0530

    Add localStorage support to persist chat messages

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,16 @@ import ChatMessages from "./components/ChatMessages";
 import "./App.css";
 
 function App() {
-  const [chatMessages, setChatMessages] = useState([]);
-
+  const [chatMessages, setChatMessages] = useState(
+    JSON.parse(localStorage.getItem("messages")) || [],
+  );
   // const[chatMessages,setChatMessages]=array;
   // const chatMessages = array[0];
   // const setChatMessages = array[1];
+
+  useEffect(() => {
+    localStorage.setItem("messages", JSON.stringify(chatMessages));
+  }, [chatMessages]);
 
   useEffect(() => {
     chatbot.addResponses({
